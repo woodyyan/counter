103e31cb7a438851175343a5ef910cbdcf3b2e52
Songbai Yan <colorguitar@hotmail.com>
2019-09-01 14:43:06 +0800

    finish counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="counter">
+    <h1>{{ msg }}</h1>
+    <p>这是一个counter</p>
+    <button v-on:click="clickPlus">{{click}}</button>
+    <br/>
+    <p>{{ number }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+                number: 0,
+                click: "点我"  
+    }
+        },
+        methods:{
+            clickPlus: function (){
+                this.number ++;
+            }
+        }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
